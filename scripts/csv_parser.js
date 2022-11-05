48a288a8012b29386b51500f25c6796257c419c0
joão <74837451+slocknad@users.noreply.github.com>
2022-11-05 17:20:28 +0000

    Initial commit

---
+++ scripts/csv_parser.js
@@ -0,0 +1,42 @@
+fetch('applications.csv')
+   .then(r => r.text())
+   .then(t => {
+        csv_content = t;
+        var info_string = csv_content.substring(67);
+        info_string = info_string.replaceAll(/"\w, *(\w|\w\w|\w, \w\w \w\w)"/g, 0);
+        const all_info = info_string.match(/".+"(?=,")|(?<=,+)".+"(?=,)|(?<![^,\n])[^,\n]*(?=,)|(?<=,)[^,\n]*/g);
+        var i = 0;
+        var organized_info = new Array();
+        var temp_info = new Object();
+        for (info of all_info) {
+            switch (i++) {
+                case 0:
+                    temp_info = new Object();
+                    temp_info['plate'] = info;
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    if (info[0] == '"' && info[info.length - 1] == '"') info = info.slice(1, -1);
+                    if (!info.length || info == "NO MICRO") info = "No information provided by customer."
+                    info = info.charAt(0).toUpperCase() + info.slice(1).toLowerCase();
+                    temp_info['customer'] = info;
+                    break;
+                case 3:
+                    if (info[0] == '"' && info[info.length - 1] == '"') info = info.slice(1, -1);
+                    info = info.charAt(0).toUpperCase() + info.slice(1).toLowerCase();
+                    temp_info['dmv'] = info;
+                    break;
+                case 4:
+                    temp_info['result'] = info == "Y";
+                    organized_info.push(temp_info);
+                    i = 0;
+                    break;
+                default:
+                    console.log();
+            }
+        }
+        shuffle(organized_info);
+        displayPlate(organized_info);
+   }
+);
---
+++ scripts/main.js
@@ -0,0 +1,58 @@
+var plates_info = new Array();
+var curr_index = 0;
+var guessed = 0;
+
+//Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+  
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+  
+
+function displayPlate(info) {
+    if (!plates_info.length) plates_info = info;
+
+    const cus_dmv = document.getElementById("cus-dmv");
+    cus_dmv.innerHTML = "Customer: " + plates_info[curr_index]['customer'] + "<p>DMV: " + plates_info[curr_index]['dmv'];
+
+    const plate = document.getElementById("plate");
+    plate.innerHTML = plates_info[curr_index]['plate'];
+
+    const buttons = document.getElementById("buttons");
+    buttons.innerHTML = "<div class=\"d-grid gap-2\"><button type=\"button\" class=\"btn btn-success\" onclick=\"checkAnswer(true)\">Accepted</button><button type=\"button\" class=\"btn btn-danger\" onclick=\"checkAnswer(false)\">Denied</button></div>"
+}
+
+function checkAnswer(bool) {
+    const accepted = plates_info[curr_index++]['result'];
+    const cus_dmv = document.getElementById("cus-dmv");
+
+    if (accepted)
+        cus_dmv.innerHTML += "<div class=\"accepted\">Verdict: Accepted</div>";
+    else cus_dmv.innerHTML += "<div class=\"denied\">Verdict: Denied</div>";
+
+    var beginning;
+    if (accepted == bool) {
+        guessed++;
+        beginning = "<h5>Correct! (";
+    }
+    else beginning = "<h5>Wrong! (";
+
+    const percentage = Math.round(guessed / curr_index * 100 * 100) / 100;
+    const buttons = document.getElementById("buttons");
+    const guessed_bar = document.getElementById("guessed");
+    buttons.innerHTML = beginning + percentage + "% correct answers so far)</h1><button type=\"button\" class=\"btn btn-primary\" onclick=\"displayPlate(info)\">Continue</button>";
+    guessed_bar.innerHTML = "Plates guessed:" + guessed + "/" + curr_index;
+
+}
